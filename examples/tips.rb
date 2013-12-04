97cd0b21e4c9920c3cae6c5842a3c85b6fa94c51
jeromeboe <jeromeboe@gmail.com>
2013-12-04 14:48:33 +0100

    tips example

--- examples/tips.rb
+++ examples/tips.rb
@@ -3,7 +3,7 @@ require File.join(dir, 'fuzzyfic')
 
 # Variable input 1 : Service quality
 service_quality = {
-	bad: Fuzzyfic::Trapezoid.new([0,3], [0,2]),
+	bad: Fuzzyfic::Trapezoid.new([0,3], [0,2]), # => support=[0,3], kernel=[0,2]
 	good: Fuzzyfic::Trapezoid.new([3,7], [4,6]),
 	excellent: Fuzzyfic::Trapezoid.new([7,10], [6,10])
 }
@@ -16,7 +16,7 @@ food = {
 
 # Variable output : Tips
 tips = {
-	low: Fuzzyfic::Triangle.new([0,10], 5),
+	low: Fuzzyfic::Triangle.new([0,10], 5), # => support=[0,10], kernel/peak=5
 	average: Fuzzyfic::Triangle.new([10,20], 15),
 	high: Fuzzyfic::Triangle.new([20,30], 25)
 }
@@ -24,19 +24,10 @@ tips = {
 
 # Rules
 rules = []
-rules.push service_quality[:bad].or(food[:execrable]).then tips[:low]
+rules.push service_quality[:bad].or(food[:execrable]).then tips[:low] # => If the service_quality is bad or the food is execrable, then the tips are low
 rules.push service_quality[:good].then tips[:average]
 rules.push service_quality[:excellent].or(food[:delicious]).then tips[:high]
 
 
-# Compute
-mat = Array.new(10){ Array.new(10){0.0} }
-for i in (0...10)
-	for j in (0...10)
-		v = Fuzzyfic::Defuzzifier.cog(rules, i, j)
-		puts "Nan : "+i.to_s+", "+j.to_s if v.nan?
-		puts "Nil : "+i.to_s+", "+j.to_s if v.nil?
-		mat[i][j] = v.round 2
-	end
-end
-p mat
+# Apply Rules, using Centroid/COG method
+Fuzzyfic::Defuzzifier.cog(rules, 6.19, 8.42)# Print 19.85 => (service_quality=6.19, food=8.42) => tips=19.85
